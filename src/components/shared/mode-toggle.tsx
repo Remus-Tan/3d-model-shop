03dcd2d30339ba9556d870f10f689a5999110ce4
Remus <Remusthings@gmail.com>
2024-02-08 02:30:07 +0800

    Move dark mode component to shared folder

---
+++ src/components/shared/mode-toggle.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+
+export default function ModeToggle() {
+    const { theme, setTheme } = useTheme();
+
+    return (
+        <TooltipProvider delayDuration={100}>
+            <Tooltip>
+                <TooltipTrigger>
+                    <div className="border border-border rounded-md p-2" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+                        <Sun className="text-foreground h-[1.2rem] w-[1.2rem] transition-all dark:hidden" />
+                        <Moon className="text-foreground h-[1.2rem] w-[1.2rem] transition-all hidden dark:block" />
+                    </div>
+                </TooltipTrigger>
+                <TooltipContent className="min-w-max">
+                    Switch between Light and Dark modes.
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    );
+}
